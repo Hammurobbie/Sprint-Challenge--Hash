bcf4087aeb470e5e018adb2fcbdec223bac5e3d7
Robbie Porter <rthomporter@gmail.com>
2020-05-08 11:39:39 -0500

    1,2,4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,5 +1,4 @@
 def has_negatives(a):
-
     """
     YOUR CODE HERE
     """
@@ -8,4 +7,22 @@ def has_negatives(a):
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1,-2,1,2,3,4,-4]))
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+
+def has_negatives(a):
+    cache = {}
+    result = []
+
+    for num in a:
+        if abs(num) in cache:
+            cache[abs(num)] = 1
+            result.append(abs(num))
+        else:
+            cache[abs(num)] = 0
+
+    return result
+
+
+if __name__ == "__main__":
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
