f7c8e8c667589a20006a87db4a79efc317c0a132
Robbie Porter <rthomporter@gmail.com>
2020-05-08 15:55:05 -0500

    brute force passing on ex 5

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,23 +3,24 @@ def intersection(arrays):
     cache = {}
     result = []
 
-    for i in range(10):
+    for i in range(len(arrays) + 1):
         if i in arrays[0]:
-            cache[i] = 1
+            cache[str(i)] = 1
+            print(cache)
     for i in range(len(arrays) - 1):
-        if cache[1] in arrays[i]:
-            cache[1] += 1
-        if cache[2] in arrays[i]:
-            cache[2] += 1
-        if cache[3] in arrays[i]:
-            cache[3] += 1
+        if cache["1"] in arrays[i]:
+            cache["1"] += 1
+        if cache["2"] in arrays[i]:
+            cache["2"] += 1
+        if cache["3"] in arrays[i]:
+            cache["3"] += 1
+        print(cache)
 
     for i in cache.items():
-        print(i[1])
         if i[1] > 1:
-            result.append(i[0])
+            result.append(int(i[0]))
 
-    print(cache)
+    print(cache["2"])
     return result
 
 
