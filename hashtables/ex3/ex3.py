e7883d7540eb0f72d97ac4fe55768d08f6a196d8
Robbie Porter <rthomporter@gmail.com>
2020-05-08 12:58:20 -0500

    almost 3?

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,31 @@
 def intersection(arrays):
 
-    """
-    YOUR CODE HERE
-    """
+    cache = {}
+    result = []
 
+    for i in range(10):
+        if i in arrays[0]:
+            cache[i] = 1
+    for i in range(len(arrays) - 1):
+        if cache[1] in arrays[i]:
+            cache[1] += 1
+        if cache[2] in arrays[i]:
+            cache[2] += 1
+        if cache[3] in arrays[i]:
+            cache[3] += 1
+
+    for i in cache.items():
+        print(i[1])
+        if i[1] > 1:
+            result.append(i[0])
+
+    print(cache)
     return result
 
 
 if __name__ == "__main__":
-    arrays = []
-
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays = [[1, 2, 3],
+              [1, 4, 5],
+              [1, 6, 7]]
 
     print(intersection(arrays))
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,9 +1,19 @@
 def finder(files, queries):
 
-    """
-    YOUR CODE HERE
-    """
+    result = []
+    cache = {}
 
+    for i in files:
+        if i in cache:
+            pass
+        else:
+            cache[i] = i
+    for i in files:
+        for j in queries:
+            if j in i:
+                result.append(i)
+
+    print(cache)
     return result
 
 
